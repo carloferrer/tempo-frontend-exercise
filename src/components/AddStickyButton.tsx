310d78b9f345e46fcff6d3aed062af47b0881241
Carlo Ferrer <thecarloferrer@gmail.com>
2025-09-23 20:35:19 -0400

    Add "add-sticky" section (#5)
    * Add sticky section

---
+++ src/components/AddStickyButton.tsx
@@ -0,0 +1,40 @@
+import { useMemo } from 'react';
+import { type TDispatchStickies, type TStickies } from '../App';
+
+interface AddStickyButtonProps {
+  newNote: string;
+  size: 200 | 300;
+  stickies: TStickies;
+  dispatchStickies: TDispatchStickies;
+}
+
+export const AddStickyButton = ({
+  newNote,
+  size,
+  stickies,
+  dispatchStickies,
+}: AddStickyButtonProps) => {
+  const mouseAtCenterShift = useMemo(() => size / 2, [size]);
+  const newId = useMemo(() => Object.keys(stickies).length, [stickies]);
+
+  return (
+    <a
+      onClick={({ clientX, clientY }) => {
+        dispatchStickies({
+          type: 'add',
+          id: newId,
+          note: newNote,
+          size,
+          position: {
+            x: clientX - mouseAtCenterShift,
+            y: clientY - mouseAtCenterShift,
+          },
+          onTheMove: true,
+        });
+      }}
+      style={{ marginLeft: 10, padding: 10, border: 'solid black 1px' }}
+    >
+      + Size-{size} Sticky
+    </a>
+  );
+};
